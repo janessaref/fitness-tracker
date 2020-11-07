6f812939d4ef25c2de2d118442afc0fbac00b671
Game Over <janessarefong@gmail.com>
2020-11-06 17:21:36 -0800

    fix last woworkout display

--- models/Workout.js
+++ models/Workout.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const WorkoutSchema = new Schema({
-    exercise: {
+    exercises: {
         type: Array,
         default: [],
     },
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -8,20 +8,20 @@ module.exports = function(app) {
 
     app.get("/api/workouts", (req, res) => {
         Workout.find({}).sort({ _id: -1 }).limit(1)
-            .then((data) => {
+            .then(data => {
                 res.send(data);
             })
-            .catch((err) => {
+            .catch(err => {
                 console.log(err);
             });
     });
 
     app.put("/api/workouts/:id", (req, res) => {
-        Workout.findByIdAndUpdate(req.params.id, { $push: { exercise: req.body } })
-            .then((data) => {
+        Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } })
+            .then(data => {
                 res.send(data);
             })
-            .catch((err) => {
+            .catch(err => {
                 console.log(err);
             });
     });
@@ -29,15 +29,15 @@ module.exports = function(app) {
     app.post("/api/workouts", (req, res) => {
         Workout.create(req.body)
             .then(dbWorkout => {
-                res.json(dbWorkout);
+                res.send(dbWorkout);
             })
             .catch(err => {
                 res.status(400).json(err);
             });
     });
 
-    // app.get("/api/workouts/range", ({ body }, res) => {
-    //     Workout.find(body)
+    // app.get("/api/workouts/range", (req, res) => {
+    //     Workout.find({})
     //         .then(dbWorkout => {
     //             res.json(dbWorkout);
     //         })
